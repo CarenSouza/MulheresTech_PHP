e8cd078564076bfee024ac9b5ac031fc8e172575
Programação PHP 36129522025.3 <36129522025.3@senacrjedu.local>
2025-06-16 10:12:51 -0300

    Adicionei aula 07 e atualizei aula 06

---
+++ 07 Aula 16.06.25.js
@@ -0,0 +1,36 @@
+// Loop While
+// usado para interação com o usuário condição de verificação na entrada
+
+
+let i = 0
+while(i < 6){ 
+    console.log(i)
+i++
+}
+
+// do while
+
+j=0
+
+do{
+    console.log(j)
+j++
+}while(j < 6)
+
+// Loop For
+
+for(k=0; j<6; k++){
+consonle.log(k)
+}
+
+
+
+
+
+
+
+
+
+
+
+
