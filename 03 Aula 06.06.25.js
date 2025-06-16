a2e41fe41d01b413600c34dbd094c0a37a90de36
Programação PHP 36129522025.3 <36129522025.3@senacrjedu.local>
2025-06-16 08:38:31 -0300

    Adicionei os arquivos das aulas

---
+++ 03 Aula 06.06.25.js
@@ -0,0 +1,12 @@
+// Desconto da Passagem do salario da pessoa
+
+
+let salario = 1000
+let passagem = 6
+
+// let desconto = salario*6/100
+
+
+
+let desconto2 = salario*passagem/100
+console.log('O desconto da passagem é: ' + desconto2)
---
+++ 05 Aula 11.06.25.js
@@ -0,0 +1,7 @@
+numero = 5
+texto = '5'
+
+
+if(numero === texto){
+    console.log("Numero igual a texto")
+}
